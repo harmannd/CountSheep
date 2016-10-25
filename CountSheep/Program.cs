11532c27687711de641fe22a69cf9b891a413810
harmannd <derekhar2468@gmail.com>
2016-10-25 18:21:54 -0500

    Set up classes

---
+++ CountSheep/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.CountSheep
+{
+    //
+    //Contains the list of number that were counted.
+    //
+    public class list
+    {
+
+    }
+
+    class CountSheep
+    {
+        static void Main(string[] args)
+        {
+            int interval = 0;
+            Console.WriteLine( "Input" + "Output".PadLeft(10) + "\r\n" + "\r\n" );
+            while ( true )
+            {
+                try
+                {
+                    interval = Console.Read();
+                    if (interval > 0 && interval < 101)
+                        break;
+                }
+                catch ( Exception )
+                {
+                    Console.WriteLine("Invalid entry");
+                }
+            }
+        }
+
+        public void Count( int interval )
+        {
+
+        }
+    }
+}
