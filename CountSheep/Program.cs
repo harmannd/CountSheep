f8c3ecaf02627d54a8445f31b968c2363f5601e2
harmannd <derekhar2468@gmail.com>
2016-10-27 18:43:08 -0500

    Fully Compiled

--- CountSheep/Program.cs
+++ CountSheep/Program.cs
@@ -3,41 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace MyProject.CountSheep
 {
-    //
-    //Contains the list of number that were counted.
-    //
-    public class list
-    {
-
-    }
-
     class CountSheep
     {
+        private static List<char> digits;
+        private static int MAX = int.MaxValue;
+        private static int LARGE = (int)Math.Pow(10,6);
+
         static void Main(string[] args)
         {
             int interval = 0;
-            Console.WriteLine( "Input" + "Output".PadLeft(10) + "\r\n" + "\r\n" );
-            while ( true )
+            int start = 0;
+            string str = null;
+            StreamReader inFile = new StreamReader("C:\\Users\\Derek\\Downloads\\A-large-practice.in");
+            StreamWriter outFile = new StreamWriter("C:\\Users\\Derek\\Downloads\\outlarge.txt");
+
+            str = inFile.ReadLine();
+            int.TryParse(str, out interval);            
+            for ( int i = 0; i < interval; i++ )
             {
-                try
+                str = inFile.ReadLine();
+                bool isNumeric = int.TryParse(str, out start);
+                if (isNumeric && start >= 0 && start <= LARGE)
                 {
-                    interval = Console.Read();
-                    if (interval > 0 && interval < 101)
-                        break;
+                    int last = countDigits(start);
+                    int caseNum = i + 1;
+                    if (last == -1)
+                        outFile.WriteLine("Case #" + caseNum + ": INSOMNIA");
+                    else
+                        outFile.WriteLine("Case #" + caseNum + ": " + last);
                 }
-                catch ( Exception )
-                {
-                    Console.WriteLine("Invalid entry");
-                }
+                else
+                    i--;
             }
+            outFile.Flush();
+            outFile.Close();
         }
 
-        public void Count( int interval )
+        public static int countDigits(int start)
         {
+            int current = start;
+            string str;
+            int i = 0;
+            digits = new List<char>();
 
+            if (start == 0)
+                return -1;
+            while ( current <=  MAX )
+            {
+                str = current.ToString();
+                var strArray = str.ToCharArray();
+                foreach ( char c in strArray )
+                {
+                    if (!digits.Contains(c))
+                        digits.Add(c);
+                }
+                if (digits.Count == 10)
+                    return current;
+                current = (i + 1) * start;
+                i++;
+            }
+            return -1;
         }
     }
 }
